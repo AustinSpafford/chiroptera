5c3bfec7b5b9fb72eb383aa065a9303cf9ecf9c9
Austin Spafford <austin.spafford@gmail.com>
2016-03-23 16:01:50 -0700

    Initial commit
    Just the bare bones starter, but it's reading from the microphone,
    though GetSpectrumData() is apparently a dead-end because it's
    apparently been bugged since Unity 5.2.

---
+++ Assets/EchoLight/MicrophoneCapture.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+
+public class MicrophoneCapture : MonoBehaviour
+{
+	public string MicrophoneDeviceName = null;
+
+	public void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+
+		ConnectMicrophoneToAudioSource();
+	}
+
+	public void Update()
+	{
+		StartPlaybackWhenReady();
+
+		if (audioSource.isPlaying)
+		{
+			float[] microphoneSamples = new float[8];
+
+			audioSource.GetSpectrumData(
+				microphoneSamples,
+				0, // channel
+				FFTWindow.BlackmanHarris);
+
+			// Debug message.
+			{
+				var debugMessage = new System.Text.StringBuilder();
+
+				foreach (float sample in microphoneSamples)
+				{
+					debugMessage.Append(sample);
+					debugMessage.Append(", ");
+				}
+
+				Debug.Log(debugMessage);
+			}
+		}
+	}
+	
+	private AudioSource audioSource = null;
+
+	private void ConnectMicrophoneToAudioSource()
+	{
+		string[] existingDeviceNames = Microphone.devices;
+
+		MicrophoneDeviceName = (
+			(existingDeviceNames.Length > 0) ? 
+				existingDeviceNames[0] :
+				null);	
+		
+		if (MicrophoneDeviceName != null)
+		{
+			audioSource.clip =
+				Microphone.Start(
+					MicrophoneDeviceName,
+					true, // loop
+					1, // lengthSec
+					AudioSettings.outputSampleRate);
+
+			// We must loop in order to continuously record.
+			audioSource.loop = true;
+			
+			// NOTE: We'll begin playback once the microphone captures its initial samples.
+		}
+	}
+
+	private void StartPlaybackWhenReady()
+	{
+		// If the microphone finally captured its first set of samples, then begin playback.
+		// Otherwise, if we had attempted to immediately play the audio source, we'd wind up
+		// perceiving a very long delay, specifically equal to the ring buffer's entire length.
+		if (!audioSource.isPlaying &&
+			(Microphone.GetPosition(MicrophoneDeviceName) > 0))
+		{
+			audioSource.Play();
+		}
+	}
+}
+
